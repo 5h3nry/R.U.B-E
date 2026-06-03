ca023f4d12ea264ff1947b301aad20661358f88b
5h3nry <40074857+5h3nry@users.noreply.github.com>
2026-06-03 15:30:36 +0100

    Cleanup

--- script.js
+++ script.js
@@ -48,15 +48,11 @@ function dashboard() {
   return `<h1>Welcome, ${user}.</h1>
       <nav class="site clear">
       <ul>
-      <li><a href="google.com" id="docs">Documents</a></li>
-      <li><a href="google.com" id="vids">Video Logs</a></li>
-      <li><a href="google.com" id="voice">Voice Logs</a></li>
+      <li><a onClick="renderDocuments" id="docs">Documents</a></li>
+      <li><a onClick="renderDocuments" id="vids">Video Logs</a></li>
+      <li><a onClick="renderDocuments" id="voice">Voice Logs</a></li>
       </ul>
       </nav>
       <div id="console">
       </div>`;
-}
-
-function renderDocuments() {
-  return '<p>Project War Horse<br />R.U.B-E<br />1999-09-19</p><p><br />TO: Roehampton Research Grant Applications<br />FROM: A.Stoddard<br />SUBJECT: A return to orthodox methods.</p><p>To whom it may concern,</p><p>I come to you today with an exciting new proposal.<br />With new advancements in gene editing technology we want to create a new wave of war machines.</p><p>Horses.</p><p>Strong, resilient creatures with such wonderful elegance.<br />With your funding we want to engineer an advanced breed that runs faster, needs less sleep, consumes less resources.<br />Once our greatest ally in the war they have since been replaced by flawed mechanical counterparts - for true greatness we require a return to orthodox methods.</p><p>We will make them perfect weapons of flesh.</p><p><br />- A.Stoddard</p>'
 }
